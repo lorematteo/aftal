a18432a00840026240d88308e162b3135d59a62a
Mattéo Lo Re <loremattteo@gmail.com>
2022-11-18 15:55:54 +0100

    Firebase integration iOS/Android 🔥

--- src/Main/signin.js
+++ src/Main/signin.js
@@ -1,16 +1,45 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
 
 import { COLORS } from '../../utils/constants';
 
+import auth from '@react-native-firebase/auth';
+
 export default function SignScreen(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignUp = async () => {
+        await auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user)
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
 
     return (
-        <View style={styles.Appcontainer}>
-            <Text>Se connecter</Text>
-            <TouchableOpacity>
-                <Text>S'inscrire</Text>
-            </TouchableOpacity>
-        </View>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Email" value={email} onChangeText={text => setEmail(text)} style={styles.input}/>
+                <TextInput placeholder="Password" value={password} onChangeText={text => setPassword(text)} style={styles.input} secureTextEntry/>
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={() => { }} style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutLine]}>
+                    <Text style={styles.buttonOutLineText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -22,4 +51,49 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.primary,
         padding: 30,
     },
-  });
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+    },
+    inputContainer: {
+        width: '80%'
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonOutLine: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#0782F9',
+         borderWidth: 2,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+    buttonOutLineText: {
+        color: '#0782F9',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+});
